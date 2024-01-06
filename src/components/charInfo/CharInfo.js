d9de371e8240aedf3dc7289784db749eb04f6213
Akhmat <light95@list.ru>
2024-01-06 14:21:33 +0800

    loki to LOKI commit

--- src/components/charInfo/CharInfo.js
+++ src/components/charInfo/CharInfo.js
@@ -24,7 +24,7 @@ const CharInfo = () => {
       </div>
 
       <div className="char__descr">
-      In Norse mythology, Loki is a god or jötunn (or both). Loki is the son
+      In Norse mythology, LOKI is a god or jötunn (or both). LOKI is the son
         of Fárbauti and Laufey, and the brother of Helblindi and Býleistr. By
         the jötunn Angrboða, Loki is the father of Hel, the wolf Fenrir, and the
         world serpent Jörmungandr. By Sigyn, Loki is the father of Nari and/or
