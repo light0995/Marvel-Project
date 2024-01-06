0c8d2cdfd02600e2796b3432a3ed3174e3fd72f5
Akhmat <light95@list.ru>
2024-01-06 14:24:20 +0800

    test commit1

--- src/components/charInfo/CharInfo.js
+++ src/components/charInfo/CharInfo.js
@@ -24,12 +24,12 @@ const CharInfo = () => {
       </div>
 
       <div className="char__descr">
-      In Norse mythology, LOKI is a god or jötunn (or both). LOKI is the son
+      In Norse mythology, LOKI is a god or jötunn (or both). LOKI is the SON
         of Fárbauti and Laufey, and the brother of Helblindi and Býleistr. By
         the jötunn Angrboða, Loki is the father of Hel, the wolf Fenrir, and the
         world serpent Jörmungandr. By Sigyn, Loki is the father of Nari and/or
-        Narfi and with the stallion Svaðilfari as the father, Loki gave birth—in
-        the form of a mare—to the eight-legged horse Sleipnir. In addition, Loki
+        Narfi and with the stallion Svaðilfari as the father, LOKI gave birth—in
+        the form of a mare—to the eight-legged horse Sleipnir. In addition, LOKI
         is referred to as the father of Váli in the Prose Edda.
       </div>
 
