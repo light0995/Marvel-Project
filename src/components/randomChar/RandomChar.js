40fbf8b49f79605808c802535958b58536051c36
Akhmat <light95@list.ru>
2024-01-06 20:55:42 +0800

    test3

--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -1,90 +1,190 @@
 
-import mjolnir from "../../resources/img/mjolnir.png";
+
+
+import './randomChar.scss';
 import thor from '../../resources/img/thor.jpeg';
-import MarvelService from "../../services/MarvelService";
-import Spinner from "../spinner/Spinner";
-
-import "./randomChar.scss";
-import { Component } from "react";
-
+import mjolnir from '../../resources/img/mjolnir.png';
+import { Component } from 'react';
+import MarvelService from '../../services/MarvelService';
 
 class RandomChar extends Component {
+
   constructor (props) {
-    super (props);
+    super(props);
     this.updateChar();
-  };
+  }
 
   state = {
-    char: {},
-    loading: true,
-  };
-
-  onCharLoaded = (char) => {
-    this.setState({char})
+    name: null,
+    description: null,
+    thumbnail: null,
+    homepage: null,
+    wiki: null,
   }
+
 
   marvelService = new MarvelService();
   updateChar = () => {
-    const id = Math.floor(Math.random() * (1011400 - 1011000) + 1011000);
+    const id = Math.floor(Math.random () * (1011400 - 1011000) + 1011000);
     this.marvelService
     .getCharacter(id)
-    .then(this.onCharLoaded)
+    .then(res => {
+      const result = res.data.results[0];
+      this.setState({
+        name: result.name,
+        description: result.description,
+        thumbnail: result.thumbnail.path + '.' + result.thumbnail.extension,
+        homepage: result.urls[0].url,
+        wiki: result.urls[1].url,
+      })
+    })
   }
-  
+
 
   render () {
-    const {char, loading} = this.state;
-
-    if (loading) {
-      return <Spinner/>
-    }
+    console.log(this.state.name)
+    const {name, description, thumbnail, homepage, wiki} = this.state;
     return (
       <div className="randomchar">
-        {loading ? <Spinner/> : <View char={char}/>}
-      <div className="randomchar__static">
-        <p className="randomchar__title">Random character for today!<br/>
-        Do you want to get to know him better?</p>
-        <p className="randomchar__title">
-          Or choose another one
-        </p>
-
-        <button className="button button__main">
-          <div className="inner">try it</div>
-        </button>
-
-        <img src={mjolnir} alt="mjolnir" className="randomchar__decoration"/>
-
-
+          <div className="randomchar__block">
+              <img src={thumbnail} alt="Random character" className="randomchar__img"/>
+              <div className="randomchar__info">
+                  <p className="randomchar__name">{name}</p>
+                  <p className="randomchar__descr">{description}</p>
+                  <div className="randomchar__btns">
+                      <a href="#" className="button button__main">
+                          <div className="inner" onClick={homepage}>homepage</div>
+                      </a>
+                      <a href="#" className="button button__secondary">
+                          <div className="inner" onClick={wiki}>wiki</div>
+                      </a>
+                  </div>
+              </div>
+          </div>
+          <div className="randomchar__static">
+              <p className="randomchar__title">
+                  Random character for today!<br/>
+                  Do you want to get to know him better?
+              </p>
+              <p className="randomchar__title">
+                  Or choose another one
+              </p>
+              <button className="button button__main">
+                  <div className="inner">try it</div>
+              </button>
+              <img src={mjolnir} alt="mjolnir" className="randomchar__decoration"/>
+          </div>
       </div>
-
-      </div>
-    )
-  }
-};
-
-
-const View = (char) => {
-
-
-const {name, description, thumbnail, homepage, wiki} = char;
-  return (
-    <div className="randomchar__block">
-    <img src={thumbnail} alt="thor" className="randomchar__img"/>
-    <div className="randomchar__info">
-      <p className="randomchar__name">{name}</p>
-      <p className="randomchar__descr">{description}</p>
-      <div className="randomchar__btns">
-        <a href={homepage} className="button button__main">
-          <div className="inner">Homepage</div>
-        </a>
-        <a href={wiki} className="button button__secondary">
-          <div className="inner">wiki </div>
-        </a>
-      </div>
-    </div>
-  </div>
-
   )
+  }
 }
 
 export default RandomChar;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import mjolnir from "../../resources/img/mjolnir.png";
+// import thor from '../../resources/img/thor.jpeg';
+// import MarvelService from "../../services/MarvelService";
+// import Spinner from "../spinner/Spinner";
+
+// import "./randomChar.scss";
+// import { Component } from "react";
+
+
+// class RandomChar extends Component {
+//   constructor (props) {
+//     super (props);
+//     this.updateChar();
+//   };
+
+//   state = {
+//     char: {},
+//     loading: true,
+//   };
+
+//   onCharLoaded = (char) => {
+//     this.setState({char})
+//   }
+
+//   marvelService = new MarvelService();
+//   updateChar = () => {
+//     const id = Math.floor(Math.random() * (1011400 - 1011000) + 1011000);
+//     this.marvelService
+//     .getCharacter(id)
+//     .then(this.onCharLoaded)
+//   }
+  
+
+//   render () {
+//     const {char, loading} = this.state;
+
+ 
+//     return (
+//       <div className="randomchar">
+//         {loading ? <Spinner/> : <View char={char}/>}
+//       <div className="randomchar__static">
+//         <p className="randomchar__title">Random character for today!<br/>
+//         Do you want to get to know him better?</p>
+//         <p className="randomchar__title">
+//           Or choose another one
+//         </p>
+
+//         <button className="button button__main">
+//           <div className="inner">try it</div>
+//         </button>
+
+//         <img src={mjolnir} alt="mjolnir" className="randomchar__decoration"/>
+
+
+//       </div>
+
+//       </div>
+//     )
+//   }
+// };
+
+
+// const View = (char) => {
+
+
+// const {name, description, thumbnail, homepage, wiki} = char;
+//   return (
+//     <div className="randomchar__block">
+//     <img src={thumbnail} alt="thor" className="randomchar__img"/>
+//     <div className="randomchar__info">
+//       <p className="randomchar__name">{name}</p>
+//       <p className="randomchar__descr">{description}</p>
+//       <div className="randomchar__btns">
+//         <a href={homepage} className="button button__main">
+//           <div className="inner">Homepage</div>
+//         </a>
+//         <a href={wiki} className="button button__secondary">
+//           <div className="inner">wiki </div>
+//         </a>
+//       </div>
+//     </div>
+//   </div>
+
+//   )
+// }
+
+// export default RandomChar;
--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -1,39 +1,82 @@
 
 
 class MarvelService {
-
     _apiBase = `https://gateway.marvel.com:443/v1/public/`;
     _apiKey = `apikey=b2e33643d53718da3c5300b24675496d`;
 
     getResource = async (url) => {
-        const res = await fetch (url);
+        let res = await fetch(url);
+    
         if (!res.ok) {
-            throw new Error (`Could not fetch ${url}, status: ${res.status}`);
-        };
-        
+            throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+        }
+    
         return await res.json();
-    };
+    }
 
-    getAllCharacters = async () => {
-        const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
-        return res.data.results.map(this._transformCharacter)
-    };
+    getAllCharacters = () => {
+        return this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+    }
 
-    getCharacter = async (id) => {
-        const res = await this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
-        return this._transformCharacter(res.data.results[0]);
-    };
-
-    _transformCharacter = (character) => {
-        return {
-            name: character.name,
-            description: character.description === '' ? 'Описание не найдено' : character.description,
-            thumbnail: character.thumbnail.path + '.' + character.thumbnail.extension,
-            homepage: character.urls[0].url,
-            wiki: character.urls[1].url,
-        };
-    };
-};
-
+    getCharacter = (id) => {
+        return this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
+    }
+}
 
 export default MarvelService;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class MarvelService {
+
+    // _apiBase = `https://gateway.marvel.com:443/v1/public/`;
+    // _apiKey = `apikey=b2e33643d53718da3c5300b24675496d`;
+
+//     getResource = async (url) => {
+//         const res = await fetch (url);
+//         if (!res.ok) {
+//             throw new Error (`Could not fetch ${url}, status: ${res.status}`);
+//         };
+        
+//         return await res.json();
+//     };
+
+//     getAllCharacters = async () => {
+//         const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+//         return res.data.results.map(this._transformCharacter)
+//     };
+
+//     getCharacter = async (id) => {
+//         const res = await this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
+//         return this._transformCharacter(res.data.results[0]);
+//     };
+
+//     _transformCharacter = (character) => {
+//         return {
+//             name: character.name,
+//             description: character.description === '' ? 'Описание не найдено' : character.description,
+//             thumbnail: character.thumbnail.path + '.' + character.thumbnail.extension,
+//             homepage: character.urls[0].url,
+//             wiki: character.urls[1].url,
+//         };
+//     };
+// };
+
+
+// export default MarvelService;
